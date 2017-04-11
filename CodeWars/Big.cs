b137890a47cde2f9448f955d7ed82c0465c30851
GeorgeSeeger <GeorgeSeeger@BARUCH>
2017-04-11 08:47:18 +0100

    Added solution for adding big ints as strings

---
+++ CodeWars/Big.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Big
+    {
+        public static string Add(string a, string b)
+        {
+            var c = a.PadLeft(b.Length, '0');
+            var d = b.PadLeft(a.Length, '0');
+            string answer = string.Empty;
+            bool carryTheOne = false;
+
+            for (int i = d.Length - 1; i >= -1; i--)
+            {
+                if (i >= 0)
+                {
+                    var digit = int.Parse(c.Substring(i, 1)) + int.Parse(d.Substring(i, 1));
+                    if (carryTheOne)
+                    {
+                        digit += 1;
+                    }
+                    if (digit >= 10)
+                    {
+                        carryTheOne = true;
+                        digit -= 10;
+                    }
+                    else
+                    {
+                        carryTheOne = false;
+                    }
+                    answer += digit.ToString();
+                }
+                else
+                {
+                    answer += carryTheOne ? "1" : "";
+                }
+                
+            }
+            return string.Join("", answer.Reverse());
+        }
+
+//        public static void Main(string[] args)
+//        {
+//            Big.Add("444", "123456");
+//        }
+    }
+}
