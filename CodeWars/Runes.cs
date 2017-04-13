2ca408089632206b80c2aa1622eb3c4051c97b24
GeorgeSeeger <GeorgeSeeger@BARUCH>
2017-04-13 09:07:43 +0100

    Solved Decode the Rune

--- CodeWars/Runes.cs
+++ CodeWars/Runes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,35 @@ namespace CodeWars
         //https://www.codewars.com/kata/546d15cebed2e10334000ed9/train/csharp
         public static int solveExpression(string expression)
         {
-            int missingDigit = -1;
+            var statements = expression.Split('=');
+            var sum = statements.First();
+            var result = statements.Last();
+            var digits = "0123456789".Where(c => !expression.Contains(c)).ToList();
+            foreach (var digit in digits)
+            {
+                if (Evaluate(sum.Replace('?', digit)) == Evaluate(result.Replace('?', digit)))
+                {
+                    if (!(digit == '0' && result.Replace('?', '0').Contains("00")))
+                    {
+                        return int.Parse(digit.ToString());
+                    }
+                }
+            }
+            return -1;
+        }
 
-            //Write code to determine the missing digit or unknown rune
-            //Expression will always be in the form
-            //(number)[opperator](number)=(number)
-            //Unknown digit will not be the same as any other digits used in expression
+        private static int Evaluate(string expression)
+        {
+            var loDataTable = new DataTable();
+            var loDataColumn = new DataColumn("Eval", typeof(int), expression);
+            loDataTable.Columns.Add(loDataColumn);
+            loDataTable.Rows.Add(0);
+            return (int)(loDataTable.Rows[0]["Eval"]);
+        }
 
-            return missingDigit;
+        public static void Main(string[] args)
+        {
+            
         }
     }
 }
