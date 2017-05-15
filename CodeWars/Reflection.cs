b429e440572ace851ac26362d0239dab8359c192
GeorgeSeeger <GeorgeSeeger@BARUCH>
2017-05-15 08:45:04 +0100

    Some more reflection katas

--- CodeWars/Reflection.cs
+++ CodeWars/Reflection.cs
@@ -27,25 +27,32 @@ namespace CodeWars
         {
             if (testObject == null) return string.Empty;
             var type = testObject.GetType();
-            var methods = type.GetMethods();
-            var strings = methods.Where(mi => mi.DeclaringType == type && mi.ReturnType.IsInstanceOfType("") && mi.GetParameters().Length == 0)
-                .Select(mi => (string) mi.Invoke(testObject, new object[] {})).GroupBy(s => s.Length)
-                .OrderByDescending(g => g.Key)
-                .Select(g => g.OrderBy(s => s))
-                .SelectMany(g => g);
+            var strings = type.GetFields()
+                              .Where(pi => pi.FieldType == typeof(string))
+                              .Select(pi => (string)pi.GetValue(testObject))
+                              .Concat(type.GetMethods()
+                                          .Where(mi => mi.DeclaringType == type && mi.ReturnType.IsInstanceOfType("") && mi.GetParameters().Length == 0)
+                                          .Select(mi => (string) mi.Invoke(testObject, new object[] {})))
+                                .GroupBy(s => s.Length)
+                                .OrderByDescending(g => g.Key)
+                                .Select(g => g.OrderBy(s => s))
+                                .SelectMany(g => g);
             return string.Join("", strings);
         }
 
         [Fact]
         public static void MemberResultsTest()
         {
-            Assert.Equal("Test-OutputStark", ConcatStringMembers(new Refl()));
+            Assert.Equal("Test-OutputStarkTen", ConcatStringMembers(new Refl()));
             Assert.Equal("OutputIt", ConcatStringMembers(new testClass()));
             Assert.Equal("", ConcatStringMembers(null));
         }
 
         public class Refl
         {
+
+            public string B = "Ten";
+
             public string Output()
             {
                 return "Test-Output";
