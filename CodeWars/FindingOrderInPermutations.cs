202a76aa91b1a505db3db598cd5de94b4e1d7120
GeorgeSeeger <GeorgeSeeger@BARUCH>
2017-06-22 14:24:00 +0100

    Permutations have been conquered

---
+++ CodeWars/FindingOrderInPermutations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeWars
+{
+    public class FindingOrderInPermutations
+    {
+        public string MiddlePermutation(string s)
+        {
+            var sorted = s.OrderBy(c => c).ToList();
+            var middleIndex = Factorial(s.Length) / 2;
+            var middleWord = "";
+            var index = BigInteger.Zero;
+            while (middleWord.Length < s.Length) { 
+                    foreach (var c in sorted) {
+                        if (index + OrderedStartsWith(sorted, c) >= middleIndex) {
+                            middleWord += c;
+                            sorted.Remove(c);
+                            break;
+                        }
+                        index += OrderedStartsWith(sorted, c);
+                    }
+                
+            }
+            return middleWord;
+        }
+
+        private BigInteger Factorial(int n)
+        {
+            return Enumerable.Range(1, n).Aggregate(BigInteger.One, (total, i) => BigInteger.Multiply(total, i));
+        }
+
+        private BigInteger OrderedStartsWith(List<char> sorted, char c)
+        {
+            return Factorial(sorted.Count - 1);
+        }
+
+        [Fact]
+        public void Tests()
+        {
+            Assert.Equal("bac", MiddlePermutation("abc"));
+            Assert.Equal("bdca", MiddlePermutation("abcd"));
+            Assert.Equal("cbxda", MiddlePermutation("abcdx"));
+            Assert.Equal("cxgdba", MiddlePermutation("abcdxg"));
+            Assert.Equal("dczxgba", MiddlePermutation("abcdxgz"));
+        }
+}
+}
--- CodeWars/Permutation.cs
+++ CodeWars/Permutation.cs
@@ -15,7 +15,7 @@ namespace CodeWars
         {
             perms = new List<string>();
         }
-        private  void Swap(ref char a, ref char b)
+        private void Swap(ref char a, ref char b)
         {
             if (a == b) return;
 
@@ -30,6 +30,46 @@ namespace CodeWars
             int x = list.Length - 1;
             GetPer(list, 0, x);
             return perms;
+        }
+
+        public List<string> HeapsPermutationAlgorithm(string s)
+        {
+            var c = new int[s.Length];
+            var perms = new List<string> { s };
+
+            var i = 0;
+            while (i < s.Length)
+            {
+                if (c[i] < i)
+                {
+                    if (i % 2 == 0)
+                    {
+                        perms.Add(SwapStr(perms.Last(), 0, i));
+                    }
+                    else
+                    {
+                        perms.Add(SwapStr(perms.Last(), c[i], i));
+                    }
+                    c[i]++;
+                    i = 0;
+                }
+                else
+                {
+                    c[i] = 0;
+                    i++;
+                }
+            }
+            return perms;
+        }
+
+        private string SwapStr(string s, int i, int j)
+        {
+            var chars = s.ToCharArray();
+            var temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            return new string(chars);
+
         }
 
         private  void GetPer(char[] list, int k, int m)
