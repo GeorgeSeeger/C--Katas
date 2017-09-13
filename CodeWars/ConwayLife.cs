deede952cf542b835f0b03651a24c05f21e40381
GeorgeSeeger <GeorgeSeeger@BARUCH>
2017-09-13 09:03:25 +0100

    Still rubbishing with game of life but close

--- CodeWars/ConwayLife.cs
+++ CodeWars/ConwayLife.cs
@@ -3,76 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CodeWars {
     public class ConwayLife {
         private readonly int[,] cells;
         private int[,] newField;
+        public int[,] Cells => newField;
 
         public ConwayLife(int[,] cells) {
             this.cells = cells;
             this.newField = new int[cells.GetLongLength(0) + 2, cells.GetLongLength(1) + 2];
         }
 
-        public int[,] NextGeneration() {
+        public ConwayLife NextGeneration() {
             for (var i = 0; i <= newField.GetUpperBound(0); i++) {
                 for (var j = 0; j <= newField.GetUpperBound(1); j++) {
                     var neighbours = this.GetNeighbours(i, j);
-                    if (!IsOverCrowded(neighbours) && !IsUnderCrowded(neighbours)) newField[i, j] = 1;
+                    if (ItLives(neighbours, this.GetCellAt(i, j))) newField[i, j] = 1;
                 }
             }
             CropNewField();
-            return this.cells;
+            return new ConwayLife(this.newField);
+        }
+
+        private int GetCellAt(int i, int j) {
+            if (i < 1 || i + 1 > this.cells.GetUpperBound(0) || j < 1 || j + 1 > this.cells.GetUpperBound(1)) return 0;
+            return this.cells[i, j];
+        }
+
+        private bool ItLives(int[] neighbours, int cell) {
+            if (cell == 1 && !IsUnderCrowded(neighbours) && !IsOverCrowded(neighbours)) return true;
+            return neighbours.Count(k => k == 1) == 3 && cell == 0;
         }
 
         private void CropNewField() {
-            var coords = new List<int>();
-            //top
+            int top = 0;
+            int bottom = 0;
+            int left = 0;
+            int right = 0;
             for (int i = 0; i <= newField.GetUpperBound(0); i++) {
                 var row = new List<int>();
                 for (int j = 0; j <= newField.GetUpperBound(1); j++ ){
                     row.Add(this.newField[i, j]);
                 }
                 if (row.Contains(1)) {
-                    coords.Add(i);
+                    bottom = i;
                     break;
                 }
             }
-            //bottom
             for (int i = newField.GetUpperBound(0); i >= 0; i--) {
                 var row = new List<int>();
                 for (int j = 0; j <= newField.GetUpperBound(1); j++) {
                     row.Add(this.newField[i, j]);
                 }
                 if (row.Contains(1)) {
-                    coords.Add(i);
+                    top = i;
                     break;
                 }
             }
-            //right
             for (int i = newField.GetUpperBound(1); i >= 0; i--) {
                 var row = new List<int>();
                 for (int j = 0; j <= newField.GetUpperBound(0); j++) {
                     row.Add(this.newField[j, i]);
                 }
                 if (row.Contains(1)) {
-                    coords.Add(i);
+                    right = i;
                     break;
                 }
             }
-            //left
             for (int i = 0; i <= newField.GetUpperBound(1); i++) {
                 var row = new List<int>();
                 for (int j = 0; j <= newField.GetUpperBound(0); j++) {
                     row.Add(this.newField[j, i]);
                 }
                 if (row.Contains(1)) {
-                    coords.Add(i);
+                    left = i;
                     break;
                 }
             }
-
-
+            if (new[] {top, bottom, left, right}.Any(i => i > 0)) {
+                var temp = new int[top - bottom + 1, right - left + 1];
+                int k = 0;
+                int l = 0;
+                for (int i = bottom; i <= top; i++) {
+                    for (int j = left; j <= right; j++) {
+                        temp[k, l] = newField[i, j];
+                        l++;
+                    }
+                    l = 0;
+                    k++;
+                }
+                newField = temp;
+            }
         }
 
         private bool IsUnderCrowded(IEnumerable<int> neighbours) {
@@ -85,20 +108,32 @@ namespace CodeWars {
 
         private int[] GetNeighbours(int i, int j) {
             var neighbours = new List<int>();
-            for (var k = i - 1; k <= i + 1; k++) {
-                for (var l = j - 1; l <= j + 1; l++) {
-                    if (k != i && l != j) neighbours.Add(this.cells[k, l]);
+            var rowLBound = i - 2 < 0 ? 0 : i - 2;
+            var rowUBound = i > this.cells.GetUpperBound(0) ? this.cells.GetUpperBound(0) : i ;
+            var colLBound = j - 2 < 0 ? 0 : j - 2;
+            var colUBound = j > this.cells.GetUpperBound(1) ? this.cells.GetUpperBound(1) : j;
+            for (var k = rowLBound; k <= rowUBound; k++) {
+                for (var l = colLBound; l <= colUBound; l++) {
+                    if (k + 1 != i || l + 1 != j) neighbours.Add(this.cells[k, l]);
                 }
             }
             return neighbours.ToArray();
         }
 
         public static int[,] GetGeneration(int[,] cells, int generation) {
-            var field = cells;
+            var field = new ConwayLife(cells);
             for (var i = 0; i < generation; i++) {
-                field = new ConwayLife(field).NextGeneration();
+                field.NextGeneration();
             }
-            return field;
+            return field.Cells;
+        }
+    }
+
+    public class ConwayLifeTests {
+        [Fact]
+        public void Test() {
+            var test = new ConwayLife(new int[,] {{1, 0, 0}, {0, 1, 1}, {1, 1, 0}}).NextGeneration();
+            Assert.Equal(new int[,] { { 0, 1, 0 }, { 0, 0, 1 }, { 1, 1, 1 } }, test.Cells);
         }
     }
 }
--- CodeWars/Pokerhand.cs
+++ CodeWars/Pokerhand.cs
@@ -5,32 +5,27 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeWars
-{
-    public enum Result
-    {
+namespace CodeWars {
+    public enum Result {
         Win,
         Loss,
         Tie
     }
 
-    public class PokerHand
-    {
+    public class PokerHand {
         private long Strength;
 
         private Card HighestCard;
 
         public Card[] Hand;
 
-        public PokerHand(string hand)
-        {
+        public PokerHand(string hand) {
             Hand = hand.Split(' ').Select(s => new Card(s)).ToArray();
             HighestCard = Hand.First(c => c.Value == Hand.Max(cd => cd.Value));
             Strength = CalculateStrength();
         }
 
-        private long CalculateStrength()
-        {
+        private long CalculateStrength() {
             if (IsStraightFlush()) return 0x100000000 * HighestCard.Value;
             if (IsFourOfAKind()) return 0x10000000 * (long)GetHighestValueOfKind(4);
             if (IsFullHouse()) return 0x1000000 * GetHighestValueOfKind(3);
@@ -42,70 +37,56 @@ namespace CodeWars
             return HighestCard.Value;
         }
 
-        private int GetHighestValueOfKind(int count)
-        {
+        private int GetHighestValueOfKind(int count) {
             return Hand.GroupBy(c => c.Value).Where(g => g.Count() == count).Max(g => g.Key);
         }
-        private bool IsStraightFlush()
-        {
+        private bool IsStraightFlush() {
             return IsFlush() && IsStraight();
         }
 
-        private bool IsFlush()
-        {
+        private bool IsFlush() {
             return Hand.GroupBy(c => c.Suit).Count() == 1;
         }
 
-        private bool IsStraight()
-        {
+        private bool IsStraight() {
             var vals = Hand.Select(c => c.Value).OrderBy(i => i);
-            return vals.Zip( vals.Skip(1), (x, y) => x - y).Count(x => x == -1) == 4;
+            return vals.Zip(vals.Skip(1), (x, y) => x - y).Count(x => x == -1) == 4;
         }
 
-        private bool IsFourOfAKind()
-        {
+        private bool IsFourOfAKind() {
             return Hand.GroupBy(c => c.Value).Max(g => g.Count()) == 4;
         }
 
-        private bool IsThreeOfAKind()
-        {
+        private bool IsThreeOfAKind() {
             return Hand.GroupBy(c => c.Value).Max(g => g.Count()) == 3;
         }
 
-        private bool IsFullHouse()
-        {
+        private bool IsFullHouse() {
             return IsThreeOfAKind() && IsOnePair();
         }
 
-        private bool IsOnePair()
-        {
+        private bool IsOnePair() {
             return Hand.GroupBy(c => c.Value).Count(g => g.Count() == 2) != 0;
         }
 
-        private bool IsTwoPair()
-        {
+        private bool IsTwoPair() {
             return Hand.GroupBy(c => c.Value).Count(g => g.Count() == 2) == 2;
         }
 
-        public Result CompareWith(PokerHand hand)
-        {
-            if (hand.Strength > this.Strength)
-            {
+        public Result CompareWith(PokerHand hand) {
+            if (hand.Strength > this.Strength) {
                 return Result.Loss;
             }
-            if (hand.Strength == this.Strength)
-            {
+            if (hand.Strength == this.Strength) {
                 return TieBreaker(hand);
             }
             return Result.Win;
         }
 
-        private Result TieBreaker(PokerHand hand)
-        {
+        private Result TieBreaker(PokerHand hand) {
             var thisHand = this.Hand.Select(c => c.Value).OrderByDescending(i => i);
             var thatHand = hand.Hand.Select(c => c.Value).OrderByDescending(i => i);
-            foreach (var couple in thisHand.Zip(thatHand, (i1, i2) => new {This = i1, That = i2}))
-            {
+            foreach (var couple in thisHand.Zip(thatHand, (i1, i2) => new { This = i1, That = i2 })) {
                 if (couple.This > couple.That) return Result.Win;
                 if (couple.That > couple.This) return Result.Loss;
             }
@@ -113,20 +94,16 @@ namespace CodeWars
         }
     }
 
-    public class Card
-    {
+    public class Card {
         public string Suit { get; private set; }
         public int Value { get; private set; }
-        public Card(string card)
-        {
+        public Card(string card) {
             Suit = card.Substring(1, 1);
             Value = GetValue(card.Substring(0, 1));
         }
 
-        private int GetValue(string num)
-        {
-            switch (num)
-            {
+        private int GetValue(string num) {
+            switch (num) {
                 case "A":
                     return 14;
                 case "K":
